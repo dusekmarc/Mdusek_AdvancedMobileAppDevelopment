18f904bd9852dd53dd892a9a11da1de724d01308
dusekmarc <marc.dusek@gmail.com>
2017-06-04 17:26:32 -0500

    update file path

--- Week_2_Activity/WebAppActivity_Week2/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This is an example of config.php
-$dbhost = 'us-cdbr-azure-southcentral-f.cloudapp.net';
-$dbuser = 'b943fa01545dee';
-$dbpass = 'eb878929';
-$dbname = 'mdusekactivityone';
-?>
--- Week_2_Project/ProjectPlan_Week2/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbhost = 'us-cdbr-azure-southcentral-f.cloudapp.net';
-$dbuser = 'b45ce0a1558071';
-$dbpass = '8544e943';
-$dbname = 'mdusekprojectappdb';
-?>
